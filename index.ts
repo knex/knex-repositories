e91a8e975b40c60e8f92b23aa7d298cbbd24fb5d
Igor Savin <iselwin@gmail.com>
2021-08-22 00:18:40 +0300

    Cleanup

--- index.ts
+++ index.ts
@@ -1,8 +1,2 @@
-export { checkHeartbeat, HEARTBEAT_QUERIES } from './lib/heartbeatUtils'
-
-export { copyWithoutUndefined, pick, isEmptyObject, groupBy } from './lib/objectUtils'
-
-export { chunk } from './lib/chunkUtils'
-
-export { updateJoinTable, calculateEntityListDiff } from './lib/diffUtils'
-export type { UpdateJoinTableParams, EntityListDiff } from './lib/diffUtils'
+export { AbstractRepository } from './lib/AbstractRepository'
+export type { RepositoryConfig } from './lib/AbstractRepository'
