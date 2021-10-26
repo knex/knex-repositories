ea7addacaaae1d0e0d3feb580b3b5ccdf2e3f454
Igor Savin <iselwin@gmail.com>
2021-10-26 03:02:30 +0300

    add getByCriteriaForUpdate (#33)

--- test/integration/repositories/UserRepositoryLite.ts
+++ test/integration/repositories/UserRepositoryLite.ts
@@ -1,16 +1,11 @@
 import { AbstractRepository } from '../../../lib/AbstractRepository'
 import { Knex } from 'knex'
-import { Filters, FullUserRow, NewUserRow, UpdatedUserRow } from './UserRepository'
+import { FullUserRow, NewUserRow, UpdatedUserRow } from './UserRepository'
 
-export type UserRepositoryLite = AbstractRepository<
-  NewUserRow,
-  FullUserRow,
-  UpdatedUserRow,
-  Filters
->
+export type UserRepositoryLite = AbstractRepository<NewUserRow, FullUserRow, UpdatedUserRow>
 
 export function createUserRepositoryLite(knex: Knex): UserRepositoryLite {
-  return new AbstractRepository<NewUserRow, FullUserRow, UpdatedUserRow, Filters>(knex, {
+  return new AbstractRepository<NewUserRow, FullUserRow, UpdatedUserRow>(knex, {
     tableName: 'users',
     idColumn: 'userId',
   })
